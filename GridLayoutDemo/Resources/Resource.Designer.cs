81ed3c9f8027cdc0ad3ade9a1713f0bad703c764
Ali <ali62n62@yahoo.com>
2017-07-06 17:21:03 +0430

    audio files added

--- GridLayoutDemo/Resources/Resource.Designer.cs
+++ GridLayoutDemo/Resources/Resource.Designer.cs
@@ -57,6 +57,46 @@ namespace GridLayoutDemo
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f060001
+			public const int button1 = 2131099649;
+			
+			// aapt resource value: 0x7f060002
+			public const int button2 = 2131099650;
+			
+			// aapt resource value: 0x7f060003
+			public const int button3 = 2131099651;
+			
+			// aapt resource value: 0x7f060004
+			public const int button4 = 2131099652;
+			
+			// aapt resource value: 0x7f060005
+			public const int button5 = 2131099653;
+			
+			// aapt resource value: 0x7f060006
+			public const int button6 = 2131099654;
+			
+			// aapt resource value: 0x7f060007
+			public const int button7 = 2131099655;
+			
+			// aapt resource value: 0x7f060008
+			public const int button8 = 2131099656;
+			
+			// aapt resource value: 0x7f060000
+			public const int gridLayout1 = 2131099648;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
@@ -73,14 +113,51 @@ namespace GridLayoutDemo
 			}
 		}
 		
+		public partial class Raw
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int doyouspeakenglish = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int goodevening = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int hello = 2130968578;
+			
+			// aapt resource value: 0x7f040003
+			public const int howareyou = 2130968579;
+			
+			// aapt resource value: 0x7f040004
+			public const int ilivein = 2130968580;
+			
+			// aapt resource value: 0x7f040005
+			public const int mynameis = 2130968581;
+			
+			// aapt resource value: 0x7f040006
+			public const int please = 2130968582;
+			
+			// aapt resource value: 0x7f040007
+			public const int welcome = 2130968583;
+			
+			static Raw()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Raw()
+			{
+			}
+		}
+		
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int ApplicationName = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int Hello = 2131034112;
 			
 			static String()
 			{
